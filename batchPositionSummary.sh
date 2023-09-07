bf2af8d88d2cc29c5d870597c3cf5982342c71d5
samirbrd <samirbrd@gmail.com>
2023-09-07 09:30:07 +0530

    msgs

--- batchPositionSummary.sh
+++ batchPositionSummary.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
-cd 'G:\\My Drive\\Trading\\PositionSummary'
-
+cd 'G:\\My Drive\\Trading\\Position Summary'
+sleep 10
 git add .
+sleep 10
 git commit -m "msgs"
+sleep 10
 git push
+sleep 10
